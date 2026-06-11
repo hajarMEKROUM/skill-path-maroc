04dd620e047bd53b34ff73f2456b41de09f2d0d6
Hajar MEKROUM <hajarmekroum9@gmail.com>
2026-06-11 05:15:13 +0100

    fix: allow hosts in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    allowedHosts: true, // 👈 Cette ligne magique autorise toutes les URLs sur Railway !
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
